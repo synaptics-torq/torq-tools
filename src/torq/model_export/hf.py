1e6dd355d303c4a565d8d9f422e75bfe5c22083e
spal-synaptics <sauryadeep.pal@synaptics.com>
2026-03-17 13:56:57 -0700

    Add SPDX license identifiers

--- src/torq/model_export/hf.py
+++ src/torq/model_export/hf.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright © 2025 Synaptics Incorporated.
+
 import os
 from pathlib import Path
 
--- src/torq/model_export/onnx.py
+++ src/torq/model_export/onnx.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+# SPDX-FileCopyrightText: Copyright © 2025 Synaptics Incorporated.
+
 import json
 import logging
 import os
