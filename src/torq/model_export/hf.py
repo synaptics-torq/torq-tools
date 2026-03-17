1bb7821370437fffbfc637b0f463a8b6433ef604
spal-synaptics <sauryadeep.pal@synaptics.com>
2026-03-17 10:32:40 -0700

    Add model export base classes

---
+++ src/torq/model_export/hf.py
@@ -0,0 +1,61 @@
+import os
+from pathlib import Path
+
+from huggingface_hub import hf_hub_download
+from subprocess import check_output, CalledProcessError, STDOUT
+
+__all__ = [
+    "hf_download_models",
+    "optimum_export_onnx",
+]
+
+
+def hf_download_models(
+    repo: str,
+    components: dict[str, str],
+    subfolder: str,
+    local_dir: str | os.PathLike,
+):
+    for comp_model_name in components.values():
+        hf_hub_download(
+            repo,
+            comp_model_name,
+            subfolder=subfolder,
+            local_dir=local_dir,
+        )
+
+
+def optimum_export_onnx(
+    onnx_dir: str | os.PathLike,
+    model: str,
+    dtype: str,
+    models: list[str],
+    *,
+    opset: int = 22,
+    opt_level: str | None = "O1",
+):
+    if all(
+        (Path(onnx_dir) / name).exists()
+        for name in models
+    ):
+        return
+    cmd = [
+        "optimum-cli", "export", "onnx",
+        str(onnx_dir),
+        "--model", model,
+        "--dtype", dtype,
+        "--opset", str(opset),
+    ]
+    if opt_level:
+        cmd += ["--optimize", str(opt_level)]
+    try:
+        check_output(
+            cmd,
+            text=True,
+            stderr=STDOUT,
+        )
+    except CalledProcessError as e:
+        raise RuntimeError(
+            f"Failed to export ONNX model via '{' '.join(e.cmd)}':\n    "
+            + "\n    ".join(e.output.strip().splitlines())
+        ) from None
